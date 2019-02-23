51b327c826ea282ec47c66897cdbd201c978c434
Cristian Niño <cristiandavid113@gmail.com>
2019-02-23 12:48:31 -0500

    quite conditional to print qr

--- resources/views/recibir.blade.php
+++ resources/views/recibir.blade.php
@@ -8,7 +8,7 @@
   @endforeach --}}
   <h2 class="h3 text-info" id="userMount">${{Auth::user()->ColPesos[0]->mount}}</h2>
   <div>
-    <recibir-tansfer v-if="printer" :info-user="user"/>
+    <recibir-tansfer :info-user="user"/>
   </div>
   <input type="hidden" name="walletCOL" id="walletCOL" value="{{Auth::user()->walletCOL}}">
   <input type="hidden" name="walletBTC" id="walletBTC" value="{{Auth::user()->walletBTC}}">
